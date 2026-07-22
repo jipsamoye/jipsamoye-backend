2c4a4c0b75df8e21b9aca4a6bfebbde28a02bbc4
JeonBongsik <wjsdudtlr86@gmail.com>
2026-07-22 11:05:41 +0900

    feat: figurine 잡 엔티티·상태·리포지토리 추가

---
+++ src/main/java/com/jipsamoye/backend/domain/figurine/repository/FigurineJobRepository.java
@@ -0,0 +1,7 @@
+package com.jipsamoye.backend.domain.figurine.repository;
+
+import com.jipsamoye.backend.domain.figurine.entity.FigurineJob;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FigurineJobRepository extends JpaRepository<FigurineJob, Long> {
+}
